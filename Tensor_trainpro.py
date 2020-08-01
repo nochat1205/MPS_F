5908ec6cd7e403152d797cb527070228f0ed0ecc
nochat1205 <52604076+nochat1205@users.noreply.github.com>
2020-08-01 11:47:39 +0800

    Update Tensor_trainpro.py

--- Tensor_trainpro.py
+++ Tensor_trainpro.py
@@ -9,9 +9,6 @@ import MPS
 from math import sqrt
 
 
-# mode-(0,1)
-# just success in (I1, I2, I3, I4...)mode-0 == (I1,I2, I3I4I5...)mode-0
-# mode-1,2 可用,->更泛用
 
 
 img_eg = imageio.mimread("img/cat.gif")
@@ -19,39 +16,7 @@ tensor = np.array(img_eg)
 tensor = tensor[5:30, :, :, :3]
 print(tensor.shape)
 
-# left to right
-B = []
-r = []
-# 二维数组存储r
-
-##############################################
-M = tl.unfold(tensor, 0)
-U, Sigma, VT = np.linalg.svd(M, 0)
-r.append([Sigma.shape[0]])
-B.append(U.reshape(1, U.shape[0], -1))
-
-
-# 思路
-# SVD = I1r1 r1I...
-# r1 I2....->r1I2 I2i3..->r1I2r2 r2...
-for i in range(tensor.ndim - 2):
-    C = np.diag(Sigma).dot(VT)
-    r[i].extend(tensor.shape[i+1:])
-    M = tl.fold(C, 0, tuple(r[i]))
-    M = MPS.unfold2(M)
-    U, Sigma, VT = np.linalg.svd(M, 0)
-    r.append([Sigma.shape[0]])
-    B.append(U)
-
-
-C = np.diag(Sigma).dot(VT)
-B.append(C.reshape(C.shape[0], -1, 1))
-
-######################################################
-B[1] = tl.moveaxis(B[1], 1, 0) # 旋转轴
-B[2] = tl.moveaxis(B[2], 1, 0)
-B[1] = tl.fold(B[1], 2, (r[0][0], tensor.shape[1], r[1][0])) #->rnInrn+1
-B[2] = tl.fold(B[2], 2, (r[1][0], tensor.shape[2], r[2][0]))
+B = MPS.tensorToMPS(tensor)
 
 # 混合正则化
 B = MPS.Mix_canonical(B, 1)
